cb5adae8ec06c86774bc782f69432bf5a3739c94
Tobin Nelson <tobin.j.n@gmail.com>
2018-07-09 17:55:23 -0600

    Getting rid of stash

---
+++ script.js
@@ -0,0 +1,16 @@
+var webTitle = document.querySelector('.web-title');
+var webURL = document.querySelector('.web-url');
+var enterBtn = document.querySelector('.enter-btn');
+var titleCardOne = document.querySelector('#Title-card-one');
+var urlCardOne = document.querySelector('#Url-card-one');
+
+// var read = document.querySelector('.read-btn-one');
+// var delete = document.querySelector('.delete-btn-one');
+
+enterBtn.addEventListener('click', grabLinks);
+
+var grabLinks = function() {
+  console.log('test');
+  // titleCardOne.innerHTML = webTitle.value;
+  // urlCardOne.innerHTML = webURL.value;
+}
