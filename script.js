9902e6300f7eab70871e1c085104cc1d6bd7dcbe
sashajustice <sabrinnasreddin@gmail.com>
2018-07-09 16:51:29 -0600

    Add weblink and web url and create clear functionality

--- script.js
+++ script.js
@@ -8,20 +8,28 @@ var bookMarkSide = document.querySelector('.link-cards-wrapper');
 
 enterBtn.addEventListener('click', grabLinks);
 
+
 function grabLinks(event) {
   event.preventDefault();
   var webT = webTitle.value;
-  console.log('yippe');
   var webU = webUrl.value;
-  var bookMarkCard = ` <section class="link-one">
-          <h2 id="Title-card-one">${webT}</h2>
-          <h3 id="Url-card-one" >${webU}</h3>
-          <button class="read-btn-one">Read</button>
-          <button class="delete-btn-one">Delete</button>
-        </section>`;
+  var bookMarkCard = ` 
+    <section class="link-one">
+      <h2 id="Title-card-one">${ webT }</h2>
+      <h3 id="Url-card-one" >${ webU }</h3>
+      <button class="read-btn-one">Read</button>
+      <button class="delete-btn-one">Delete</button>
+    </section>
+  `;
 
-bookMarkSide.innerHTML += bookMarkCard
-  // titleCardOne.innerHTML = webTitle.value
-  // urlCardOne.innerHTML = webUrl.value 
+  bookMarkSide.innerHTML += bookMarkCard
 
+  deleteLinks()
 }
+
+
+function deleteLinks() {
+  webTitle.value = '';
+  webUrl.value = '';
+}
+
