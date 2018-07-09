66b9419c9a63a23000bf36b19d90275f22bf21f9
sashajustice <sabrinnasreddin@gmail.com>
2018-07-09 17:41:42 -0600

    add styling on read button

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ var webUrl = document.querySelector('.web-url');
 var enterBtn = document.querySelector('.enter-btn');
 var titleCardOne = document.querySelector('#Title-card-one');
 var urlCardOne = document.querySelector('#Url-card-one');
-var bookMarkSide = document.querySelector('.link-cards-wrapper');
+var listCardsWrapper = document.querySelector('.link-cards-wrapper');
+var readBtnOne = document.querySelector('.read-btn-one');
+
 
 
 enterBtn.addEventListener('click', grabLinks);
+listCardsWrapper.addEventListener('click', markAsRead);
 
 
 function grabLinks(event) {
@@ -22,7 +25,7 @@ function grabLinks(event) {
     </section>
   `;
 
-  bookMarkSide.innerHTML += bookMarkCard
+  listCardsWrapper.innerHTML += bookMarkCard
 
   deleteLinks()
 }
@@ -31,5 +34,26 @@ function grabLinks(event) {
 function deleteLinks() {
   webTitle.value = '';
   webUrl.value = '';
+}
+
+
+
+function markAsRead (event) {
+  // event.target()
+
+  if(event.target.className === 'read-btn-one') {
+    event.target.parentElement.classList.add('red')
+   
+     // .classList.add('go')
+     // console.log(event.target.classList)
+
+  }
+  
+
+
+
+
+
+
 }
 
